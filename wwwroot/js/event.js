28f9b8b51c51286445c3103bcad02b36865af9a3
SF3696 <gobinath.dhamotharan@syncfusion.com>
2022-09-13 10:46:53 +0530

    Feature(EJ2-61919)-FinalDiagramBuilder

--- wwwroot/js/event.js
+++ wwwroot/js/event.js
@@ -238,7 +238,7 @@ var DiagramClientSideEvents = (function () {
     DiagramClientSideEvents.prototype.dragEnter = function (args) {
         var obj = args.element;
         obj.width = 100;
-        obj.height = 70;
+        obj.height = 50;
     };
     DiagramClientSideEvents.prototype.historyChange = function (args) {
         var diagram = selectedItem.selectedDiagram;
